d329e5a2f9a9f93bdf3c4fc3075b2e7db02662c8
Micah Thompson <micahthompson@Micahs-MacBook-Pro.local>
2025-10-01 13:45:53 -0500

    UpToProblem3

--- HW3_MicahThompson.sh
+++ HW3_MicahThompson.sh
@@ -1,1 +1,34 @@
-# Assignment 3
+# NAME: Micah Thompson CS 4350 – Unix Systems Programming
+# Section Number: 001
+# Assignment Number: 3
+# Due Date: 10 / 1 / 2025 No Later than 5:15 pm
+# Extended Due Date : 10 / 6 / 2025 No Later than 5:15 pm - No late
+# assignments will be accepted
+
+set -euo pipefail
+
+
+# Creating FunScript.txt file and adding content
+cat > FunScript.txt <<EOF
+Fall 2025
+CS4350
+Section #
+001
+scripts and shell programming using
+zeus ide
+@ TEXAS STATE UNIVERSITY
+EOF
+
+
+# Output:
+
+printf "\nShell Programming by Micah Thompson\n\n"
+# Number 1
+printf "1. Creating a new file FunScript.txt.\n\n"
+# Number 2
+printf "2. The content of the created file is:\n\n"
+cat FunScript.txt
+printf "\n"
+# Number 3
+printf "3. All lines with alphabet characters: \n\n"
+
