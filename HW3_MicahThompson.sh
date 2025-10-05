b46913be7a86eeede129a43fa19f4f01c1a9f8e1
Micah Thompson <micahthompson@Micahs-MacBook-Pro.local>
2025-10-04 19:54:34 -0500

    Final-Product-Fine-Tuned

--- HW3_MicahThompson.sh
+++ HW3_MicahThompson.sh
@@ -9,7 +9,7 @@
 set -euo pipefail
 
 
-# Creating FunScript.txt file and adding content
+# Creating FunScript.txt file and adding content described in 1 and 2
 cat > FunScript.txt <<EOF
 Fall 2025
 CS4350
@@ -33,75 +33,75 @@ printf "2. The content of the created file is:\n\n"
 cat FunScript.txt
 printf '\n'
 
-# Number 3
-printf "3. All lines with alphabet characters: \n\n"
+# Number 4
+printf "4. All lines with alphabet characters: \n\n"
 grep '[[:alpha:]]' FunScript.txt
 printf  '\n'
 
-# Number 4
-printf "4. All lines with digits: \n\n"
+# Number 5
+printf "5. All lines with digits: \n\n"
 grep '[0-9]' FunScript.txt
 printf '\n'
 
-# Number 5
-printf "5. All lines with lowercase characters: \n\n"
+# Number 6
+printf "6. All lines with lowercase characters: \n\n"
 grep '[a-z]' FunScript.txt
 printf '\n'
 
-# Number 6
-printf "6. All lines with uppercase characters: \n\n"
+# Number 7
+printf "7. All lines with uppercase characters: \n\n"
 grep '[A-Z]' FunScript.txt
 printf '\n'
 
-# Number 7
-printf "7. All lines with alphanumeric characters: \n\n"
+# Number 8
+printf "8. All lines with alphanumeric characters: \n\n"
 grep '[A-Za-z0-9]' FunScript.txt
 printf '\n'
 
-# Number 8
-printf "8. All lines with punctuation characters: \n\n"
+# Number 9
+printf "9. All lines with punctuation characters: \n\n"
 grep '[[:punct:]]' FunScript.txt
 printf '\n'
 
-# Number 9
-printf "9. All lines with the letter s followed by space: \n\n"
+# Number 10
+printf "10. All lines with the letter s followed by space: \n\n"
 grep 's ' FunScript.txt
 printf '\n'
 
-# Number 10
-printf "10. All the lines with the word zeus followed by space: \n\n"
+# Number 11
+printf "11. All the lines with the word zeus followed by space: \n\n"
 grep 'zeus ' FunScript.txt
 printf '\n'
 
-# Number 11
-printf "11. All the lines that the word programming appears at
+# Number 12
+printf "12. All the lines that the word programming appears at
 the end of the line: \n\n"
 grep 'programming$' FunScript.txt || true # Script should not fail if no match
 printf '\n'
 
-# Number 12
-printf "12. All the lines that the word Fall appears at the
+# Number 13
+printf "13. All the lines that the word Fall appears at the
 beginning of the sentence: \n\n"
 grep '^Fall' FunScript.txt
 printf '\n'
 
-# Number 13
-printf "13. All the lines that have a word that starts with C and
+# Number 14
+printf "14. All the lines that have a word that starts with C and
 ends with 0: \n\n"
 grep '\bC[[:alnum:]_]*0\b' FunScript.txt
 printf '\n'
 
-# Number 14
-printf "14. All lines that contain a word that starts with s and
+# Number 15
+printf "15. All lines that contain a word that starts with s and
 end with l: \n\n"
 grep '\bs[a-zA-Z0-9]*l\b' FunScript.txt
 printf '\n'
 
-# Number 15
+# Number 16
 printf "Deleting the existing File FunScript.txt \n\n"
 rm FunScript.txt
 
-# Number 16
+# Number 17
 printf "Display the content of FunScript.txt: \n\n"
 cat FunScript.txt && true # Script should not fail if no match
 printf '\n'
